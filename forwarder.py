1db0ac64bcb97a009a48ed94bfe0dcdf252b6cf4
Hakus <altenotiz@gmail.com>
2015-03-13 16:39:35 -0700

    Finished basic implementation

--- forwarder.py
+++ forwarder.py
@@ -4,14 +4,16 @@ import threading, socket, sys, select
 CFG_NAME = "rules.conf"
 ROUTES = {}
 THREADS = []
-HOST = "127.0.0.1"
-BUF_SIZE = 1024
+HOST = ""
+BUF_SIZE = 2048
 
 class ProxyServer:
 
 	def __init__(self, port):
 		self.port = port
+		self.clients = []
 		self.destinations = {}
+		self.connected_paths = []
 
 		print "Starting server on %s port %s" % (HOST, self.port)
 		self.srv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,50 +21,38 @@ class ProxyServer:
 		self.srv_socket.bind((HOST, port))
 		self.srv_socket.listen(5)
 
-	# 	print "Starting server on %s port %s" % (HOST, self.connport)
-	# 	self.srv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# 	self.srv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	# 	self.srv_socket.bind((HOST, self.connport))
-	# 	self.srv_socket.listen(5)
-	# 	self.check_destination()
-
 	def handle_accept(self):
-		try:
-			clients = [self.srv_socket]
-			while True:
-				readList, writeList, exceptions = select.select(clients, [], [])
-				# For each socket ready to be read
-				for sockets in readList:
-					# Ready to accept another client
-					if sockets == self.srv_socket:
-						try:
-							conn, address = sockets.accept()
-							conn.setblocking(0)
-							clients.append(conn)
-							for path in ROUTES[self.port]:
-								if path[0] == address[0]:
-									self.destinations[conn] = self.connect_dest(path[1], path[2])
-									clients.append(self.destinations[conn])
-						except socket.error:
-							clients.remove(sockets)
-					else:
-						data = sockets.recv(1024)
-						socket_info = sockets.getpeername()
-						if data == "":
-							clients.remove(sockets)
-							print "%s:%s has disconnected" % socket_info
-							sockets.close()
-						else:
-							# Determine where to forward to
-							for path in ROUTES[self.port]:
-								if socket_info[0] == path[0]:
-									print "%s: %s" % (socket_info, data)
-									self.destinations[sockets].sendall(data)
+		self.clients = [self.srv_socket]
+		while True:
+			readList, writeList, exceptions = select.select(self.clients, [], [])
+			# For each socket ready to be read
+			for sockets in readList:
+				# Ready to accept another client
+				if sockets == self.srv_socket:
+					try:
+						conn, address = sockets.accept()
+						for path in ROUTES[self.port]:
+							if path[0] == address[0]:
+								self.destinations[conn] = self.connect_dest(path[1], path[2])
+								if self.destinations[conn] is False:
+									conn.close()
 									break
-								elif socket_info[0] == path[1]:
-									print "Got something back from destination"
-		finally:
-			pass
+								conn.setblocking(0)
+								self.clients.append(conn)
+								self.clients.append(self.destinations[conn])
+								self.connected_paths.append(Connected_Route(conn, self.destinations[conn]))
+								break
+					except socket.error:
+						self.clients.remove(sockets)
+				else:
+					# Determine where to forward to
+					for conn_sock in self.connected_paths:
+						if sockets == conn_sock.src:
+							self.forward_data(sockets, conn_sock.dest)
+							break
+						elif sockets == conn_sock.dest:
+							self.forward_data(sockets, conn_sock.src)
+							break
 
 	def connect_dest(self, dest_ip, dest_port):
 		print "Checking if %s:%s is available..." % (dest_ip, dest_port)
@@ -71,19 +61,41 @@ class ProxyServer:
 			dest_socket.connect((dest_ip, dest_port))
 		except socket.error:
 			print "\tCould not connect to destination server..."
-			return
+			return False
 		print "\tConnected to %s:%s" % (dest_ip, dest_port)
-
 		return dest_socket
 
-	# def handle_forward(self, from_sock, to_sock):
-	# 	data = from_sock.recv(BUF_SIZE)
-	# 	if data == "":
-	# 		print "Source connection disconnected"
-	# 		raise socket.error
-	# 	else:
-	# 		print data
-	# 		to_sock.sendall(data)
+	def forward_data(self, src_socket, dest_socket):
+		data = src_socket.recv(BUF_SIZE)
+		socket_info = src_socket.getpeername()
+		# print "Received %s from %s" % (data, socket_info)
+		if data == "":
+			print "%s:%s has disconnected" % socket_info
+			self.terminate_connection(Connected_Route(src_socket, dest_socket))
+			return
+		try:
+			dest_socket.send(data)
+		except IOError:
+			print "Destination unavailable, disconnecting sockets"
+			self.terminate_connection(Connected_Route(src_socket, dest_socket))
+
+	def terminate_connection(self, path_sockets):
+		source = path_sockets.src
+		destination = path_sockets.src
+		try:
+			self.clients.remove(source)
+			self.clients.remove(destination)
+			self.destinations.remove(source)
+			self.connected_paths.remove(path_sockets)
+		except ValueError:
+			pass
+		source.close()
+		destination.close()
+
+class Connected_Route():
+	def __init__(self, src_socket, dest_socket):
+		self.src = src_socket
+		self.dest = dest_socket
 
 def parse_config():
 	for line in file(CFG_NAME):
@@ -97,7 +109,6 @@ def parse_config():
 def main():
 	# Create config file if it doesn't exist
 	# Parse config file
-
 	parse_config()
 
 	for port, emptyVar in ROUTES.iteritems():
